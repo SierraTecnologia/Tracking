737d9678afb1189d010e2adbde4e2e6fda1c1db6
Ricardo R Sierra <ricardo@sierratecnologia.com.br>
2020-05-05 07:53:35 -0300

    Reajeitando pastas doas classses e renomeando tudo.

--- src/TrackingProvider.php
+++ src/TrackingProvider.php
@@ -19,7 +19,7 @@ use Tracking\Models\Statistics\Route as RouteBase;
 use Tracking\Models\Statistics\Device;
 use Tracking\Models\Statistics\Request;
 use Tracking\Models\Statistics\Platform;
-use Support\Helpers\Traits\Models\ConsoleTools;
+use Support\Traits\Providers\ConsoleTools;
 use Tracking\Console\Commands\MigrateCommand;
 use Tracking\Console\Commands\PublishCommand;
 use Tracking\Http\Middleware\TrackStatistics;
